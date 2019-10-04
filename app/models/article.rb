42b6cc6da87c56530a2dbe96020d8639aa5652b6
Daniel  Drumwright <danieldrumwright@gmail.com>
2019-10-04 21:33:00 +0000

    Automatically backed up by Learn

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,19 +2,4 @@
 
 class Article < ActiveRecord::Base
 
-  attr_accessor :title, :content
-
-  ARTICLES = []
-
-def initialize(arg)
-  @title = arg[:title]
-  @content = arg[:content]
-  ARTICLES << self
-end
-
-
-def self.all
-  ARTICLES
-end
-
 end
